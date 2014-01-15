a984ecacbb6fd64ddfe1675f21e348242d1e26bd
Rachel Carvalho <rachel.carvalho@gmail.com>
2014-01-15 14:53:51 -0500

    wavy terrain for simple voxel engine test

--- public/simple.js
+++ public/simple.js
@@ -2,16 +2,25 @@ var voxelEngine = require('voxel-engine');
 
 var voxelPlayer = require('voxel-player');
 
-var game = voxelEngine({
+var game = window.game = voxelEngine({
   materials: ['mars'],
   generate: function(x, y, z) {
-    return y == 0 ? 1 : 0;
+    // return y == 0 ? 1 : 0;
+    var xMod20 = Math.abs(x % 20);
+    if (
+        (y == 0)
+        || (y == 1 && (xMod20 > 0 && xMod20 < 10))
+        || (y == 2 && (xMod20 > 4 && xMod20 < 6))
+      )
+      return 1;
+    else
+      return 0;
   }
 });
 
 game.appendTo(document.body);
 
-var player = voxelPlayer(game)();
+var player = window.player = voxelPlayer(game)();
 
 player.position.set(100000, 20, 100000);
 
